c5610ea7116b42482398bd67506d96e51c3632bb
Veerash Ayyagari <veeray@microsoft.com>
2022-07-13 11:54:13 -0700

    fix formatting issues

--- src/WebJobs.Script.WebHost/Configuration/ServerlessSecurityDefenderOptionsSetup.cs
+++ src/WebJobs.Script.WebHost/Configuration/ServerlessSecurityDefenderOptionsSetup.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using Az.ServerlessSecurity.Platform;
 using Microsoft.Extensions.Options;
 
@@ -6,6 +9,7 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Configuration
     internal class ServerlessSecurityDefenderOptionsSetup : IConfigureOptions<ServerlessSecurityDefenderOptions>
     {
         private readonly IEnvironment _environment;
+        
         public ServerlessSecurityDefenderOptionsSetup(IEnvironment environment)
         {
             _environment = environment;
